18809298c44f84dbed3af492a7f71f8ecd0ea95c
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-25 17:56:18 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,9 @@
 function solution(arr) {
+    // reduce 함수로 배열의 모든 원소를 합한다.
     const answer = arr.reduce((acc, item) => {
         return acc += item;
     }, 0);
 
+    // 합산한 수를 배열의 크기로 나눠 평균값을 구한다.
     return answer / arr.length;
 }
