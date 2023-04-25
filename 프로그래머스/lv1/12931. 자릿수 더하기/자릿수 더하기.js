3d0cb8abdf0408c246cf5969bef7aa0fdcae7254
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-25 17:53:12 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,6 @@
 function solution(n) {
+    // 숫자 n을 문자열로 바꾼 후 전개 연산자로 배열로 만든다.
+    // reduce 함수 안에서 배열의 원소들을 숫자로 만든 후 합산해 리턴한다.
     return [...String(n)].reduce((acc, el) => {
         return acc += Number(el)
     }, 0);
