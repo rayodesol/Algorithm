30ee1351ca76d68b07e692e3ae26042f8e95c88e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-26 20:49:05 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,13 +1,21 @@
 function solution(s) {
-    let cnt = 0;
+    // 방법 2
+    return [...s.toLowerCase()].reduce((acc, el) => {
+        if (el === 'p') acc++;
+        if (el === 'y') acc--;
+        return acc;
+    }, 0) === 0 ? true : false;
     
-    // 문자열을 for문으로 한글자씩 순회하며
-    // P/p일 땐 카운팅을 +로, Y/y일 땐 카운팅을 -로
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'p' || s[i] === 'P') cnt += 1;
-        if (s[i] === 'y' || s[i] === 'Y') cnt -= 1;
-    }
+//     // 방법 1
+//     let cnt = 0;
     
-    // 카운팅한 결과가 0일 때만 p, y의 개수가 같을 때이다.
-    return cnt === 0 ? true : false;
+//     // 문자열을 for문으로 한글자씩 순회하며
+//     // P/p일 땐 카운팅을 +로, Y/y일 땐 카운팅을 -로
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === 'p' || s[i] === 'P') cnt += 1;
+//         if (s[i] === 'y' || s[i] === 'Y') cnt -= 1;
+//     }
+    
+//     // 카운팅한 결과가 0일 때만 p, y의 개수가 같을 때이다.
+//     return cnt === 0 ? true : false;
 }
