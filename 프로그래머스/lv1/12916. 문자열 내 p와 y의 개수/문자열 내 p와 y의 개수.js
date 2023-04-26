fb5ea0f7eb8fed3aa2574030894191cdf1a31d5b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-26 20:56:50 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,12 +1,16 @@
 function solution(s) {
-    // 방법 2
+    // 방법 2 - reduce 함수
+    // toLowerCase() 로 문자열을 모두 소문자로 바꾸고,
+    // 전개 연산자로 문자열을 배열로 만들어 reduce 함수 사용
+    // p일 땐 누산기가 +1, y일 땐 누산기가 -1
+    // 삼항연산자로 누산기 값이 0일 때 true 반환
     return [...s.toLowerCase()].reduce((acc, el) => {
         if (el === 'p') acc++;
         if (el === 'y') acc--;
         return acc;
     }, 0) === 0 ? true : false;
     
-//     // 방법 1
+//     // 방법 1 - for문
 //     let cnt = 0;
     
 //     // 문자열을 for문으로 한글자씩 순회하며
@@ -16,6 +20,6 @@ function solution(s) {
 //         if (s[i] === 'y' || s[i] === 'Y') cnt -= 1;
 //     }
     
-//     // 카운팅한 결과가 0일 때만 p, y의 개수가 같을 때이다.
+//     // 삼항연산자 사용. 카운팅한 결과가 0일 때만 p, y의 개수가 같을 때이다.
 //     return cnt === 0 ? true : false;
 }
