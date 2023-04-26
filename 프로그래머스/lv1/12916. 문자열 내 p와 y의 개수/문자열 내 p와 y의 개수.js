376789550360fd6d9da8f7c37d97eee4465dd385
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-26 20:27:31 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,10 +1,13 @@
-function solution(s){
+function solution(s) {
     let cnt = 0;
     
+    // 문자열을 for문으로 한글자씩 순회하며
+    // P/p일 땐 카운팅을 +로, Y/y일 땐 카운팅을 -로
     for (let i = 0; i < s.length; i++) {
         if (s[i] === 'p' || s[i] === 'P') cnt += 1;
         if (s[i] === 'y' || s[i] === 'Y') cnt -= 1;
     }
     
+    // 카운팅한 결과가 0일 때만 p, y의 개수가 같을 때이다.
     return cnt === 0 ? true : false;
 }
