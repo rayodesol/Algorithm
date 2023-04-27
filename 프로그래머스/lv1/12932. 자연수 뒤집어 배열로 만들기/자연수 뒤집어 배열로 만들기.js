d3eedebc3adb0bdd65bebd53eee7ac7276c7ebee
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-27 17:53:47 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,18 +1,18 @@
 function solution(n) {
-    // 방법 2
-    // n을 문자열로 만든 후, 전개 연산자로 한 글자씩 띄어 배열을 만든다.
-    // reverse 함수로 배열을 거꾸로 뒤집는다.
-    // map 함수로 원소를 모두 숫자형으로 바꾼다.
-    return [...String(n)].reverse().map(el => Number(el))
+    // // 방법 2
+    // // n을 문자열로 만든 후, 전개 연산자로 한 글자씩 띄어 배열을 만든다.
+    // // reverse 함수로 배열을 거꾸로 뒤집는다.
+    // // map 함수로 원소를 모두 숫자형으로 바꾼다.
+    // return [...String(n)].reverse().map(el => Number(el))
     
-//     // 방법 1
-//     const answer = []
+    // 방법 1
+    const answer = []
     
-//     while (n > 0) {
-//         answer.push(n % 10) // 10으로 나눈 나머지를 배열에 차례대로 넣고
-//         n = Math.floor(n / 10) // n이 0이 될 때까지 10으로 나눈 몫으로 갱신시킨다.
-//         // n = parseInt(n / 10) 으로도 가능 - 나누기의 몫을 구하는 방법
-//     }
+    while (n > 0) {
+        answer.push(n % 10) // 10으로 나눈 나머지를 배열에 차례대로 넣고
+        n = Math.floor(n / 10) // n이 0이 될 때까지 10으로 나눈 몫으로 갱신시킨다.
+        // n = parseInt(n / 10) 으로도 가능 - 나누기의 몫을 구하는 방법
+    }
     
-//     return answer
+    return answer
 }
