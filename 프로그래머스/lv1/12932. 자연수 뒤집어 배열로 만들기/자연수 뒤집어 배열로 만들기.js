a562fe36df51bf0302d7714640015cafb7d0fa35
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-27 17:37:00 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -2,8 +2,9 @@ function solution(n) {
     const answer = []
     
     while (n > 0) {
-        answer.push(n % 10)
-        n = parseInt(n / 10)
+        answer.push(n % 10) // 10으로 나눈 나머지를 배열에 차례대로 넣고
+        n = Math.floor(n / 10) // n이 0이 될 때까지 10으로 나눈 몫으로 갱신시킨다.
+        // n = parseInt(n / 10) 으로도 가능 - 나누기의 몫을 구하는 방법
     }
     
     return answer
