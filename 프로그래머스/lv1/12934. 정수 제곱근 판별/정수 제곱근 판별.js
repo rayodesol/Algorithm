6ecc285ab77d7736dc6becae151c92077c5f463b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-28 19:37:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,5 +1,7 @@
 function solution(n) {
-    const x = Math.sqrt(n);
+    const x = Math.sqrt(n); // n의 제곱근 x 구하기
     
+    // 제곱근이 정수인지 판별 - x를 parseInt를 통해 정수로 만든 것과 비교
+    // 삼항연산자로 반환값 정하기
     return x === parseInt(x) ? (x + 1) ** 2 : -1;
 }
