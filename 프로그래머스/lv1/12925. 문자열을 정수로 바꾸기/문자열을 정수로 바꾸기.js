a39adefcd85ea04bdd4cc0dcb02cafed90d21eeb
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-29 22:50:11 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,9 +1,12 @@
 function solution(s) {
     if (s[0] === '+') {
+        // 앞에 + 부호가 붙었을 때, slice 함수로 부호 제거 후, Number()로 숫자화
         return Number(s.slice(1))
     } else if (s[0] === '-') {
+        // 앞에 - 부호가 붙었을 때, slice 함수로 부호 제거 후, Number()로 숫자화 + 음수화
         return -Number(s.slice(1))
     } else {
+        // 부호가 없을 때, Number()로 숫자화
         return Number(s)
     }
 }
