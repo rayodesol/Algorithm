d17933a2263d842d4204b0c1cf768d0279867f54
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-04-30 23:52:31 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,3 +1,6 @@
 function solution(n) {
+    // n을 String()으로 문자열로 만든 후, 전개 연산자로 한글자씩 띄어 배열로 만든다.
+    // sort()로 내림차순으로 정렬한다.
+    // join()으로 배열을 문자열로 만들고, Number()로 숫자로 만든다.
     return Number([...String(n)].sort((a, b) => b - a).join(""))
 }
