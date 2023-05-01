c405c37e0621d67a5a2e02a13fb2adfd4ac89162
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-01 23:59:21 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,17 @@
+function solution(a, b) {
+    if (a === b) return a
+    
+    if (a > b) {
+        const tmp = a
+        a = b
+        b = tmp
+    }
+    
+    let sum = 0
+    
+    for (let i = a; i <= b; i++) {
+        sum += i
+    }
+    
+    return sum
+}
