6cb42fa5f3d121d974b1a64f777809499d249569
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-01 23:26:13 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,11 +1,13 @@
 function solution(x) {
-    let sum = 0
-    let i = x
+    return x % [...String(x)].reduce((acc, el) => acc += Number(el), 0) === 0 ? true : false
     
-    while (i > 0) {
-        sum += i % 10
-        i = parseInt(i / 10)
-    }
+//     let sum = 0
+//     let i = x
     
-    return x % sum === 0 ? true : false
+//     while (i > 0) {
+//         sum += i % 10
+//         i = parseInt(i / 10)
+//     }
+    
+//     return x % sum === 0 ? true : false
 }
