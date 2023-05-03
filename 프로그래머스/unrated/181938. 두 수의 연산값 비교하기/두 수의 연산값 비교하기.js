0a23ab769d19e0ab6383e2caa5fd872263decdd3
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-03 22:29:01 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,8 +1,8 @@
 function solution(a, b) {
-    const attached = Number(String(a) + String(b));
-    const calculated = 2 * a * b;
+//     const attached = Number(String(a) + String(b));
+//     const calculated = 2 * a * b;
     
-    return attached >= calculated ? attached : calculated;
+//     return attached >= calculated ? attached : calculated;
     
-    // return Number(String(a) + String(b)) >= 2 * a * b ? Number(String(a) + String(b)) : 2 * a * b;
+    return Number(String(a) + String(b)) >= 2 * a * b ? Number(String(a) + String(b)) : 2 * a * b;
 }
