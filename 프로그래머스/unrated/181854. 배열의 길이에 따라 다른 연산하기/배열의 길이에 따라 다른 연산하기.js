7857fb00f2b725ecd5b7833df55f89dcbe491a01
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-03 22:06:40 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,12 +1,16 @@
 function solution(arr, n) {
-    // 배열 길이가 짝/홀수인지에 따라 시작하는 인덱스 값을 달리 한다.
-    let start = (arr.length % 2 === 0) ? 1 : 0;
+    return arr.map((el, idx) => {
+        return (arr.length % 2 !== idx % 2) ? el + n : el;
+    })
     
-    // while문으로 인덱스를 2씩 증가하며 원소에 n을 더한다.
-    while (start < arr.length) {
-        arr[start] += n;
-        start += 2;
-    }
+//     // 배열 길이가 짝/홀수인지에 따라 시작하는 인덱스 값을 달리 한다.
+//     let start = (arr.length % 2 === 0) ? 1 : 0;
     
-    return arr;
+//     // while문으로 인덱스를 2씩 증가하며 원소에 n을 더한다.
+//     while (start < arr.length) {
+//         arr[start] += n;
+//         start += 2;
+//     }
+    
+//     return arr;
 }
