67239eee0e450950f56bb8fb1079181e0c9466e2
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-04 23:18:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,8 @@
 function solution(s) {
     // split 함수로 공백을 기준으로 s를 나눠 배열로 만든다.
-    const numArr = s.split(" ")
+    const arr = s.split(" ")
     
     // 전개 연산자와 Math.max, Math.min 함수로 최대/최소값을 구한다.
     // + 연산자로 중간에 공백까지 넣어 문자열을 리턴한다.
-    return Math.min(...numArr) + " " + Math.max(...numArr)
+    return Math.min(...arr) + " " + Math.max(...arr)
 }
