01cddae55d217ce5b85917218b36224eff99754c
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-06 01:10:37 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,9 @@
 function solution(seoul) {
-    for (let i = 0; i < seoul.length; i++) {
-        if (seoul[i] === "Kim") return `김서방은 ${i}에 있다`
-    }
+    // 방법 2 - indexOf 함수로 Kim의 위치 리턴
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`
+    
+    // // 방법 1 - for문으로 seoul의 모든 원소를 순회하며 Kim일 때 인덱스 리턴
+    // for (let i = 0; i < seoul.length; i++) {
+    //     if (seoul[i] === "Kim") return `김서방은 ${i}에 있다`
+    // }
 }
