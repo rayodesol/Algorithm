94b46bcda4d3b4c362f6d4d1b777de12f808d173
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-06 00:52:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,12 +1,14 @@
 function solution(num) {
     let cnt = 0
     
+    // 1이 되거나 반복 횟수가 500번을 넘기 전까지 무한루프
     while (true) {
-        if (num === 1) return cnt
-        if (cnt > 500) return -1
+        if (num === 1) return cnt // 1이 될 때, 반복 횟수 리턴
+        if (cnt > 500) return -1 // 반복 횟수가 500번을 넘을 때, -1 리턴
         
+        // 삼항 연산자로 짝/홀수인지에 따라 숫자 변경
         num = (num % 2 === 0) ? num / 2 : num * 3 + 1
-        cnt += 1
+        cnt += 1 // 반복 횟수 1씩 증가
     }
     
     return cnt
