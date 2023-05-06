6b22651aa501b79b39a97e6852bac5072ffc86ae
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-06 14:27:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,6 +1,7 @@
 function solution(absolutes, signs) {
     let result = 0
     
+    // 삼항 연산자로 signs의 값에 따라 absolutes 값을 더하거나 뺀다.
     for (let i = 0; i < absolutes.length; i++) {
         signs[i] ? result += absolutes[i] : result -= absolutes[i]
     }
