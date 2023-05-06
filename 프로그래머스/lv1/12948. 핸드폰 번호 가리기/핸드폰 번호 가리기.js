df71037178d4ae028f84ed11f182a03a83aadf6e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-06 14:19:59 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,5 @@
 function solution(phone_number) {
+    // repeat 함수로 *를 번호의 4자리를 뺀 만큼 반복
+    // slice 함수로 번호의 뒷 4자리 붙이기
     return "*".repeat(phone_number.length - 4) + phone_number.slice(phone_number.length - 4)
 }
