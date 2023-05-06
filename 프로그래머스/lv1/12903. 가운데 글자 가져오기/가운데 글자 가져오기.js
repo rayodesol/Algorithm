dbec5a628f1472986bbe385c862946a4a0be5eeb
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-06 15:38:13 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,8 +1,11 @@
 function solution(s) {
     const mid = s.length / 2
     
-    // 방법 2 - slice 함수 사용
-    return s.length % 2 === 0 ? s.slice(mid - 1, mid + 1) : s.slice(parseInt(mid), parseInt(mid) + 1)
+    // 방법 3
+    return s.length % 2 === 0 ? s[mid - 1] + s[mid] : s[parseInt(mid)]
+    
+    // // 방법 2 - slice 함수 사용
+    // return s.length % 2 === 0 ? s.slice(mid - 1, mid + 1) : s.slice(parseInt(mid), parseInt(mid) + 1)
     
     // // 방법 1 - splice 함수 사용
     // return s.length % 2 === 0 ? [...s].splice(mid - 1, 2).join("") : [...s].splice(parseInt(mid), 1).join("")
