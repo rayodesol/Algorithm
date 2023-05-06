f773782786a9da740aaecee026d8f3ecbea149cb
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-06 14:46:12 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,9 @@
 function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)), 1)
+    return arr.length === 0 ? [-1] : arr
+    
     // arr의 길이가 1일 경우엔 바로 [-1]를 리턴한다.
     // Math.min 함수로 arr의 최솟값을 구한 뒤,
     // filter 함수로 최솟값이 아닌 수들로만 이루어진 배열을 만든다.
-    return arr.length === 1 ? [-1] : arr.filter((el) => el !== Math.min(...arr))
+    // return arr.length === 1 ? [-1] : arr.filter((el) => el !== Math.min(...arr))
 }
