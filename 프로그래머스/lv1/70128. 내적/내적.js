0565b2bbc964f8bd14976a5981080560d17c8e83
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-07 23:09:17 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,9 +1,15 @@
 function solution(a, b) {
-    let result = 0
+    // 방법 2 - reduce 함수 사용
+    return a.reduce((acc, el, idx) => {
+        return acc += el * b[idx]
+    }, 0)
     
-    for (let i = 0; i < a.length; i++) {
-        result += a[i] * b[i]
-    }
+//     // 방법 1 - for문 사용
+//     let result = 0
     
-    return result
+//     for (let i = 0; i < a.length; i++) {
+//         result += a[i] * b[i]
+//     }
+    
+//     return result
 }
