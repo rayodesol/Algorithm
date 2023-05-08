01b8889586f44413c8dd7e1be9d2abbc4f2ce684
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-08 23:55:43 +0900

    [level 1] Title: 행렬의 덧셈, Time: 10.94 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,20 +1,20 @@
 function solution(arr1, arr2) {
-//     // 방법 2 - for문 안에 map 함수
-//     const ans = []
-    
-//     for (let i = 0; i < arr1.length; i++) {
-//         ans.push(arr1[i].map((el, idx) => el + arr2[i][idx]))
-//     }
-    
-    // 방법 1 - 이중 for문
-    // 행, 열의 크기가 같은, 모든 원소가 0인 배열로 초기화
-    const ans = Array.from(Array(arr1.length), () => Array(arr1[0].length).fill(0))
+    // 방법 2 - for문 안에 map 함수
+    const ans = []
     
     for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr1[0].length; j++) {
-            ans[i][j] = arr1[i][j] + arr2[i][j]
-        }
+        ans.push(arr1[i].map((el, idx) => el + arr2[i][idx]))
     }
+    
+//     // 방법 1 - 이중 for문
+//     // 행, 열의 크기가 같은, 모든 원소가 0인 배열로 초기화
+//     const ans = Array.from(Array(arr1.length), () => Array(arr1[0].length).fill(0))
+    
+//     for (let i = 0; i < arr1.length; i++) {
+//         for (let j = 0; j < arr1[0].length; j++) {
+//             ans[i][j] = arr1[i][j] + arr2[i][j]
+//         }
+//     }
     
     return ans;
 }
