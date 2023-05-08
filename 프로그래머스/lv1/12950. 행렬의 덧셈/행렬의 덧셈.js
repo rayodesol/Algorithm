e187da6b7fec22803ea59c7f99ed5ba1ae7b61ad
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-09 00:25:53 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.88 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -9,25 +9,25 @@ function solution(arr1, arr2) {
 //         ans.push(arr1[i].map((el, idx) => el + arr2[i][idx]))
 //     }
     
-//     // 방법 2 - 이중 for문
-//     const ans = [] // 빈 배열
-    
-//     for (let i = 0; i < arr1.length; i++) {
-//         ans[i] = []
-//         for (let j = 0; j < arr1[0].length; j++) {
-//             ans[i].push(arr1[i][j] + arr2[i][j]) // push 함수
-//         }
-//     }
-    
-    // 방법 1 - 이중 for문
-    // 행, 열의 크기가 같은, 모든 원소가 0인 배열로 초기화
-    const ans = Array.from(Array(arr1.length), () => Array(arr1[0].length).fill(0))
+    // 방법 2 - 이중 for문
+    const ans = [] // 빈 배열
     
     for (let i = 0; i < arr1.length; i++) {
+        ans[i] = []
         for (let j = 0; j < arr1[0].length; j++) {
-            ans[i][j] = arr1[i][j] + arr2[i][j] // 값 변경
+            ans[i].push(arr1[i][j] + arr2[i][j]) // push 함수
         }
     }
+    
+//     // 방법 1 - 이중 for문
+//     // 행, 열의 크기가 같은, 모든 원소가 0인 배열로 초기화
+//     const ans = Array.from(Array(arr1.length), () => Array(arr1[0].length).fill(0))
+    
+//     for (let i = 0; i < arr1.length; i++) {
+//         for (let j = 0; j < arr1[0].length; j++) {
+//             ans[i][j] = arr1[i][j] + arr2[i][j] // 값 변경
+//         }
+//     }
     
     return ans;
 }
