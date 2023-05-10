7286921fcd28588b74af3a9e44481d4004e5f273
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-10 19:43:13 +0900

    [level 1] Title: 직사각형 별찍기, Time: 44.58 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,20 +3,24 @@ process.stdin.on('data', data => {
     const input = data.split(" ");
     const n = Number(input[0]), m = Number(input[1]);
     
-    // 방법 2 - for문 1번
-    const width = "*".repeat(n) // repeat 함수로 1줄로 출력되는 가로 * 만들기
+    // 방법 3 - repeat 함수 2번
+    // \n(개행문자)까지 넣은 1줄을 repeat 함수로 m번 반복
+    console.log(`${"*".repeat(n)}\n`.repeat(m))
     
-    // for문으로 m번 반복하면서 1줄씩 출력
-    for (let i = 0; i < m; i++) {
-        console.log(width)
-    }
+//     // 방법 2 - for문 1번
+//     const width = "*".repeat(n) // repeat 함수로 1줄로 출력되는 가로 * 만들기
     
-    // // 방법 1 - 이중 for문
-    // // m이 행의 개수(세로 길이), n이 열의 개수(가로 길이)
-    // for (let i = 0; i < m; i++) {
-    //     for (let j = 0; j < n; j++) {
-    //         process.stdout.write("*") // 줄 바꿈 없이 출력
-    //     }
-    //     console.log() // 줄 바꿈
-    // }
+//     // for문으로 m번 반복하면서 1줄씩 출력
+//     for (let i = 0; i < m; i++) {
+//         console.log(width)
+//     }
+    
+//     // 방법 1 - 이중 for문
+//     // m이 행의 개수(세로 길이), n이 열의 개수(가로 길이)
+//     for (let i = 0; i < m; i++) {
+//         for (let j = 0; j < n; j++) {
+//             process.stdout.write("*") // 줄 바꿈 없이 출력
+//         }
+//         console.log() // 줄 바꿈
+//     }
 });
