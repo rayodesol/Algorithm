f8cd7bc6ae5816bd44f7a682b962171cdf1df62d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-11 23:37:23 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,6 +1,4 @@
 function solution(n, m) {
-    // const big = Math.max(n, m)
-    // const small = Math.min(n, m)
     const ans = []
     
     for (let num = n; true; num += n) {
@@ -10,16 +8,6 @@ function solution(n, m) {
         }
     }
     
-    // 최소공배수(L) 찾기 - n, m 중 큰 수를 기준으로 
-    // for (let num = big; true; num += big) {
-    //     if (num % small === 0) {
-    //         ans.push(num)
-    //         break
-    //     }
-    // }
-    
-    // A = aG, B = bG, L = abG => G = AB / L
-    // ans.unshift(big * small / ans[0])
     ans.unshift(n * m / ans[0])
     
     return ans
