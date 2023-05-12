b65f061d7b7094804c6f1459e8a391ef266bd4e1
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-13 00:19:08 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,6 +1,6 @@
 function solution(angle) {
     // 방법 2 - 삼항 연산자
-    return (0 < angle && angle < 90) ? 1 : (angle === 90) ? 2 : (90 < angle && angle < 180) ? 3 : 4
+    return (angle < 90) ? 1 : (angle === 90) ? 2 : (angle < 180) ? 3 : 4
     
     // // 방법 1 - if-else문 
     // if (0 < angle && angle < 90) {
