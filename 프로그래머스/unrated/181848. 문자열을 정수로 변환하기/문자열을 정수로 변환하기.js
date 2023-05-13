022d94cb28bb286db0511cc0b142a0eaf984a191
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-13 22:20:47 +0900

    [unrated] Title: 문자열을 정수로 변환하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.js
+++ 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.js
@@ -1,7 +1,7 @@
 function solution(n_str) {
-    // // 방법 2
-    // return parseInt(n_str)
+    // 방법 2
+    return parseInt(n_str)
     
-    // 방법 1
-    return Number(n_str)
+    // // 방법 1
+    // return Number(n_str)
 }
