a2202f28be1bb3601810d942c0801ea235098d32
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-15 16:06:18 +0900

    [level 3] Title: 정수 삼각형, Time: 37.64 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,7 +1,7 @@
 def solution(triangle):
     height = len(triangle) - 1
     
-    # 맨 아래부터 1층씩 올라가며 최댓값이 되는 합으로 값을 갱신한다.
+    # 맨 아래부터 1층씩 올라가며 최댓값이 되는 합으로 값을 갱신한다.(테뷸레이션)
     for floor in range(height, -1, -1):
         for i in range(floor):
             triangle[floor - 1][i] += max(triangle[floor][i], triangle[floor][i + 1])
