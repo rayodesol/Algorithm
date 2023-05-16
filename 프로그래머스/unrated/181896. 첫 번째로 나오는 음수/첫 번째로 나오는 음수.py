3c1ad6bc00abc3a78e53196e37a44a7af732d062
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-17 00:06:49 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,4 +1,10 @@
 def solution(num_list):
-    for i in range(len(num_list)):
-        if num_list[i] < 0: return i
+    # 방법 2 - enumerate 함수
+    for idx, el in enumerate(num_list):
+        if el < 0: return idx
     return -1
+
+    # # 방법 1 - for문
+    # for i in range(len(num_list)):
+    #     if num_list[i] < 0: return i
+    # return -1
