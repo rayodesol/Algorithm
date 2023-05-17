f8a27c7b2ec5a2bdb1e06222e9f5f53dc1f3dff8
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-18 00:37:14 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,3 +1,7 @@
 def solution(num_list):
-    num_list.reverse()
-    return num_list
+    # 방법 2 - reversed 함수; reversed 객체 반환
+    return list(reversed(num_list))
+
+    # 방법 1 - reverse 함수; 반환값 없음
+    # num_list.reverse()
+    # return num_list
