254c760e258b67e0714f767b77c6254eb768a417
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-17 23:00:07 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,6 +1,10 @@
 function solution(num_list) {
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] < 0) return i
-    }
-    return -1
+    // 방법 2 - findIndex 함수
+    return num_list.findIndex(el => el < 0)
+    
+    // // 방법 1 - for문
+    // for (let i = 0; i < num_list.length; i++) {
+    //     if (num_list[i] < 0) return i
+    // }
+    // return -1
 }
