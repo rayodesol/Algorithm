1f944735b936f5dc26064ec8012bf30e406c3b30
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-17 23:20:35 +0900

    [unrated] Title: 정수 부분, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181850. 정수 부분/정수 부분.js
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.js
@@ -1,4 +1,7 @@
 function solution(flo) {
-    // 방법 1 - parseInt
-    return parseInt(flo)
+    // 방법 2 - Math.floor
+    return Math.floor(flo)
+    
+    // // 방법 1 - parseInt
+    // return parseInt(flo)
 }
