6ecde72835103add4c9c83df3d5c846cc86bb49d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-18 00:09:36 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,5 +1,16 @@
 function solution(num_list) {
-    const multi = num_list.reduce((acc, el) => acc *= el, 1)
-    const sum = num_list.reduce((acc, el) => acc += el, 0)
+    // // 방법 2 - reduce 함수
+    // const multi = num_list.reduce((acc, el) => acc *= el, 1)
+    // const sum = num_list.reduce((acc, el) => acc += el, 0)
+    
+    // 방법 1 - for문
+    let multi = 1
+    let sum = 0
+    
+    for (num of num_list) {
+        multi *= num
+        sum += num
+    }
+    
     return multi < sum ** 2 ? 1 : 0 
 }
