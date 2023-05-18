781b5fec8e0f422b1c53b212ecf39f9eaa21552c
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-18 23:57:57 +0900

    [unrated] Title: 공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,2 +1,6 @@
 def solution(number, n, m):
-    return 1 if number % n == 0 and number % m == 0 else 0
+    # 방법 2
+    return int(number % n == 0 and number % m == 0)
+
+    # 방법 1
+    # return 1 if number % n == 0 and number % m == 0 else 0
