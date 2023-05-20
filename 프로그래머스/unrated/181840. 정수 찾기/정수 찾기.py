ee85fb03a10d2a4af7177194e21b81f3bde85ed2
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-21 00:37:20 +0900

    [unrated] Title: 정수 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.py
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.py
@@ -1,2 +1,6 @@
 def solution(num_list, n):
-    return 1 if n in num_list else 0
+    # 방법 2 - 조건문 int화
+    return int(n in num_list)
+
+    # # 방법 1 - if-else문
+    # return 1 if n in num_list else 0
