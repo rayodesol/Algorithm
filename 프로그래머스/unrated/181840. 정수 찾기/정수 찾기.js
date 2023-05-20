be58b2bc0fbf0a4d838ddcd5c7422f37a1715e5f
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-21 00:34:25 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -1,7 +1,7 @@
 function solution(num_list, n) {
-    // 방법 2 - findIndex 함수
-    return num_list.findIndex(el => el === n) !== -1 ? 1 : 0
+    // // 방법 2 - findIndex 함수
+    // return num_list.findIndex(el => el === n) !== -1 ? 1 : 0
     
-    // // 방법 1 - includes 함수
-    // return num_list.includes(n) ? 1 : 0
+    // 방법 1 - includes 함수
+    return num_list.includes(n) ? 1 : 0
 }
