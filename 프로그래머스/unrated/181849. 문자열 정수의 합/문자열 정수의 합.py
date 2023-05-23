2ec067bd42715e44b477e222fff9301937f8c14f
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-24 00:09:22 +0900

    [unrated] Title: 문자열 정수의 합, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -1,7 +1,11 @@
 def solution(num_str):
-    ans = 0
+    # 방법 2
+    return sum(map(int, list(num_str)))
+
+#     # 방법 1 - for문
+#     ans = 0
     
-    for num in list(num_str):
-        ans += int(num)
+#     for num in list(num_str):
+#         ans += int(num)
     
-    return ans
+#     return ans
