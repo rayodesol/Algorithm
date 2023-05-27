122c9fccb6ef6136ff9b30e575d10589ffbb5940
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-27 21:51:10 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,4 +1,9 @@
 function solution(sides) {
-    // (세 변 합) - 2 * (가장 긴 변) = (두 변 합) - (가장 긴 변) > 0
-    return sides.reduce((acc, el) => acc += el, 0) - 2 * Math.max(...sides) > 0 ? 1 : 2
+    // 방법 2 - sort 함수
+    sides.sort((a, b) => a - b) // 오름차순 정렬
+    return sides[0] + sides[1] > sides[2] ? 1 : 2
+    
+    // 방법 1 - reduce 함수, Math.max 함수
+    // // (세 변 합) - 2 * (가장 긴 변) = (두 변 합) - (가장 긴 변) > 0
+    // return sides.reduce((acc, el) => acc += el, 0) - 2 * Math.max(...sides) > 0 ? 1 : 2
 }
