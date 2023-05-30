1e801122a45195427b10e6768bd5e400d6a74222
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-30 20:08:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,5 +1,16 @@
 function solution(num_list) {
-    // filter 함수로 짝수 개수 구하기
-    const even = num_list.filter(el => el % 2 === 0).length
-    return [even, num_list.length - even] // 홀수 개수 = 배열 길이(전체 개수) - 짝수 개수
+    // 방법 2
+    const answer = [0, 0]
+    
+    for (num of num_list) {
+        // 2로 나눈 나머지가와 인덱스가 같다.
+        answer[num % 2]++
+    }
+    
+    return answer
+    
+    // // 방법 1
+    // // filter 함수로 짝수 개수 구하기
+    // const even = num_list.filter(el => el % 2 === 0).length
+    // return [even, num_list.length - even] // 홀수 개수 = 배열 길이(전체 개수) - 짝수 개수
 }
