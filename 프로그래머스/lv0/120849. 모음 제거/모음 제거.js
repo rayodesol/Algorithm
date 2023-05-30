e0d1994f79fba81590032976a9c216cd030a246e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-05-30 20:22:40 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,11 +1,18 @@
 function solution(my_string) {
-    let answer = '';
+    // 방법 2
+    // 전개 연산자로 문자열을 배열로 만든 뒤,
+    // filter, includes 함수로 모음이 아닌 것만 배열에 남긴다.
+    // join 함수로 배열을 문자열로 만든다.
+    return [...my_string].filter(el => !'aeiou'.includes(el)).join('')
     
-    for (letter of my_string) {
-        if (!'aeiou'.includes(letter)) {
-            answer += letter
-        }
-    }
+//     // 방법 1 - for문, includes 함수 사용
+//     let answer = '';
     
-    return answer
+//     for (letter of my_string) {
+//         if (!'aeiou'.includes(letter)) {
+//             answer += letter
+//         }
+//     }
+    
+//     return answer
 }
