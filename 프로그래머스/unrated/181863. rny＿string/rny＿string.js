acd8f9cd0c07e6446f9b5bd8456f755e4bc0b651
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-02 00:05:13 +0900

    [unrated] Title: rny_string, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.js
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -1,3 +1,7 @@
 function solution(rny_string) {
-    return rny_string.replaceAll("m", "rn")
+    // 방법 2 - reaplce, join 함수
+    return rny_string.split("m").join("rn")
+    
+    // // 방법 1 - replaceAll 함수
+    // return rny_string.replaceAll("m", "rn")
 }
