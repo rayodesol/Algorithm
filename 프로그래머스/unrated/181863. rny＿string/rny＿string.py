726570626b25bc910476722c197cf0157cf28082
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-02 00:29:28 +0900

    [unrated] Title: rny_string, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.py
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -1,2 +1,6 @@
 def solution(rny_string):
-    return 'rn'.join(rny_string.split('m'))
+    # 방법 2 - replace 함수
+    return rny_string.replace('m', 'rn')
+
+    # # 방법 1 - split, join 함수 사용
+    # return 'rn'.join(rny_string.split('m'))
