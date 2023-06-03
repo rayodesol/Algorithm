ff5240ce2a4badf909e5ed055bf17d218364f13e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-03 22:21:09 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,3 +1,6 @@
 def solution(my_string):
-    # 방법 1 - 슬라이싱
-    return my_string[::-1]
+    # 방법 2 - reversed 함수
+    return ''.join(reversed(my_string))
+
+    # # 방법 1 - 슬라이싱
+    # return my_string[::-1]
