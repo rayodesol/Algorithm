a3fa3e238dbe5a685fb3860ad72e7092a855dd50
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-05 23:34:51 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,5 +1,9 @@
 def solution(slice, n):
-    i = 1
-    while True:
-        if slice * i >= n: return i
-        i += 1
+    # 방법 2
+    return (n - 1) // slice + 1
+
+    # # 방법 1 - 피자 판 수 * 조각 수가 n이상이 될 때까지 피자 판 수를 1씩 증가시킨다.
+    # i = 1
+    # while True:
+    #     if slice * i >= n: return i
+    #     i += 1
