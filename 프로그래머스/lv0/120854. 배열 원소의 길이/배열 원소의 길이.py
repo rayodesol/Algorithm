55d34a5fb2546135cfeb92013d13b437425c7fee
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-06 21:13:01 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,2 +1,6 @@
 def solution(strlist):
-    return list(map(len, strlist)) # map 함수 사용
+    # 방법 2 - 컴프리헨션
+    return [len(str) for str in strlist]
+
+    # # 방법 1 - map 함수 사용
+    # return list(map(len, strlist))
