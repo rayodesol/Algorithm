8c1f13e29e82e5c12fdfaa1e032705e28afd0b40
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-08 00:22:37 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,11 @@
 function solution(price) {
     if (price >= 500000) {
         return Math.floor(price * 0.8)
-    } else if (price >= 300000) {
+    } else if (price >= 300000) { // else-if문으로 50만원 미만 중 30만원 이상
         return Math.floor(price * 0.9)
-    } else if (price >= 100000) {
+    } else if (price >= 100000) { // else-if문으로 30만원 미만 중 10만원 이상
         return Math.floor(price * 0.95)
-    } else {
+    } else { // 10만원 미만인 금액은 제 값을 지불해야 한다.
         return price
     }
 }
