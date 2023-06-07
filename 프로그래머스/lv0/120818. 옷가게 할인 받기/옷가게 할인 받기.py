358f981f35e7a3d0ea60fb4e9cfa74c55d81f72e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-08 02:34:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,4 +1,4 @@
-import math
+# import math
 
 def solution(price):
 #     # 방법 2 - for문, enumerate 사용
@@ -9,10 +9,10 @@ def solution(price):
         
     # 방법 1 - if-else문
     if price >= 500000:
-        return math.trunc(price * 0.8)
+        return int(price * 0.8)
     elif price >= 300000: # 50만원 미만인 것 중에 30만원 이상인 것
-        return math.trunc(price * 0.9)
+        return int(price * 0.9)
     elif price >= 100000:
-        return math.trunc(price * 0.95)
+        return int(price * 0.95)
     else:
         return price
