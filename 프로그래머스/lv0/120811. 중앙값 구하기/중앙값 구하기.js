e4316b91bc0068f1857c504cf4d11451baad8962
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-08 23:58:34 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,4 +1,4 @@
 function solution(array) {
-    array.sort((a, b) => a - b)
-    return array[parseInt(array.length / 2)]
+    // sort 함수로 정렬 후 가운데 인덱스인 수를 리턴한다.
+    return array.sort((a, b) => a - b)[parseInt(array.length / 2)]
 }
