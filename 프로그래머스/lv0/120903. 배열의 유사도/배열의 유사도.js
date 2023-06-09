6c5f87cc862b72172460dbdf2109b5de8b9ddb7d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-09 20:24:27 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,13 @@
 function solution(s1, s2) {
+    // 방법 1 - 이중 for문
     let answer = 0
     
     for (el_s1 of s1) {
         for (el_s2 of s2) {
-            if (el_s1 === el_s2) answer += 1
+            if (el_s1 === el_s2) {
+                answer += 1
+                break
+            }
         }
     }
     
