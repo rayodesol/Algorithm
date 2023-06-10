e92d01cfb4591c410b29d1b6eef1ecc2a4a06fa8
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-10 23:42:33 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,11 +1,11 @@
 def solution(s1, s2):
-    # 방법 2 - set(집합) 교집합
-    return len(set(s1) & set(s2))
+    # # 방법 2 - set(집합) 교집합
+    # return len(set(s1) & set(s2))
 
-#     # 방법 1 - for문, in 연산자
-#     answer = 0
+    # 방법 1 - for문, in 연산자
+    answer = 0
     
-#     for el_s1 in s1:
-#         if el_s1 in s2: answer += 1
+    for el_s1 in s1:
+        if el_s1 in s2: answer += 1
     
-#     return answer
+    return answer
