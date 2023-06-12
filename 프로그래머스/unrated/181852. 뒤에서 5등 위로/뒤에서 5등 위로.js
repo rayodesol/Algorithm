4ccf256723ff92bb0151500831785512c41004c8
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-12 22:04:24 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,4 +1,9 @@
 function solution(num_list) {
-    num_list.sort((a, b) => a - b) // sort 함수로 오름차순 정렬 후,
-    return num_list.filter((el, idx) => idx >= 5) // filter 함수로 인덱스가 5이상인 것만 리턴한다.
+    // 방법 2 - sort, slice 함수
+    num_list.sort((a, b) => a - b)
+    return num_list.slice(5)
+    
+    // // 방법 1 - sort, filter 함수
+    // num_list.sort((a, b) => a - b) // sort 함수로 오름차순 정렬 후,
+    // return num_list.filter((el, idx) => idx >= 5) // filter 함수로 인덱스가 5이상인 것만 리턴한다.
 }
