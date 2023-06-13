d9c0814e238ab641708ed90d74e0beb38d198c8a
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-13 23:35:24 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,5 +1,8 @@
 function solution(my_string, letter) {
-    // 방법 1 - 전개연산자, filter, join 함수
-    // 전개연산자로 배열로 만들어 filter 함수로 letter를 제외한 뒤 join 함수로 문자열로 만든다.
-    return [...my_string].filter(el => el !== letter).join('')
+    // 방법 2 - replaceAll 함수로 letter를 빈 문자열로 대체한다.
+    return my_string.replaceAll(letter, '')
+    
+    // // 방법 1 - 전개연산자, filter, join 함수
+    // // 전개연산자로 배열로 만들어 filter 함수로 letter를 제외한 뒤 join 함수로 문자열로 만든다.
+    // return [...my_string].filter(el => el !== letter).join('')
 }
