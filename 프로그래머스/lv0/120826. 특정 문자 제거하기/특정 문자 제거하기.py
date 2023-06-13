7c2b003998913d55bf0361502fb502026a552fca
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-13 23:41:16 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,2 +1,6 @@
 def solution(my_string, letter):
-    return ''.join(my_string.split(letter))
+    # 방법 2 - replace 함수
+    return my_string.replace(letter, '')
+
+    # # 방법 1 - split, join 함수
+    # return ''.join(my_string.split(letter))
