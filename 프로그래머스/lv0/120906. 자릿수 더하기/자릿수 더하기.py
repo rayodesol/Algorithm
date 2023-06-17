cc71c9688595c48f10be88ef27d081b6b4d0aba6
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-17 17:31:07 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,10 +1,10 @@
 def solution(n):
-    # 방법 2
-    return sum(map(int, str(n)))
+    # # 방법 2
+    # return sum(map(int, str(n)))
 
     # 방법 1 - while문
-    # answer = 0
-    # while n > 0:
-    #     answer += n % 10
-    #     n //= 10
-    # return answer
+    answer = 0
+    while n > 0:
+        answer += n % 10
+        n //= 10
+    return answer
