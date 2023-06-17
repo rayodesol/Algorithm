666530fcc0ddb11d29af184f4f903ac800532c1e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-17 17:37:11 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,12 @@
 function solution(n) {
-    // 방법 1 - 문자열로 바꾼 후 전개연산자로 배열로 만든 뒤, reduce 함수로 각 자리 수 합을 구한다.
-    return [...String(n)].reduce((acc, el) => acc += Number(el), 0)
+    // 방법 2 - while문으로 n을 10으로 나눈 나머지의 합을 구한다.
+    let answer = 0
+    while (n > 0) {
+        answer += n % 10
+        n = parseInt(n / 10)
+    }
+    return answer
+    
+    // // 방법 1 - 문자열로 바꾼 후 전개연산자로 배열로 만든 뒤, reduce 함수로 각 자리 수 합을 구한다.
+    // return [...String(n)].reduce((acc, el) => acc += Number(el), 0)
 }
