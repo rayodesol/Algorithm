d6545ae1c995a7f1514c736501f64e4b14667740
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-19 20:23:37 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,7 +1,7 @@
 function solution(num_list, n) {
-    // // 방법 2 - splice 함수
-    // return num_list.splice(n - 1)
+    // 방법 2 - splice 함수
+    return num_list.splice(n - 1)
     
-    // 방법 1 - slice 함수 사용. n번째이므로 인덱스로는 n-1부터 시작이다.
-    return num_list.slice(n - 1)
+    // // 방법 1 - slice 함수 사용. n번째이므로 인덱스로는 n-1부터 시작이다.
+    // return num_list.slice(n - 1)
 }
