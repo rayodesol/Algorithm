67eb51346fb7b3f00f0c0b4cead7f63b67f47ded
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-20 23:24:12 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,3 +1,7 @@
 function solution(num_list, n) {
-    return num_list.splice(0, n)
+    // 방법 2 - slice 함수
+    return num_list.slice(0, n)
+    
+    // // 방법 1 - splice 함수
+    // return num_list.splice(0, n)
 }
