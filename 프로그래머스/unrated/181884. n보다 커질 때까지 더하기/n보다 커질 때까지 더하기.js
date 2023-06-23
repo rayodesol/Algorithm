7559bce2007e1156ab6b0896fd7d111cddd19846
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-23 23:40:07 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,14 +1,14 @@
 function solution(numbers, n) {
-    // 방법 2 - reduce 함수로 n보다 커질 때까지 더한다.
-    return numbers.reduce((acc, el) => acc > n ? acc : acc += el, 0)
+    // // 방법 2 - reduce 함수로 n보다 커질 때까지 더한다.
+    // return numbers.reduce((acc, el) => acc > n ? acc : acc += el, 0)
     
-//     // 방법 1 - for문으로 배열의 원소들을 순서대로 더하고, n보다 크면 리턴한다.
-//     let answer = 0
+    // 방법 1 - for문으로 배열의 원소들을 순서대로 더하고, n보다 크면 리턴한다.
+    let answer = 0
     
-//     for (num of numbers) {
-//         if (answer > n) return answer
-//         answer += num
-//     }
+    for (num of numbers) {
+        if (answer > n) return answer
+        answer += num
+    }
     
-//     return answer
+    return answer
 }
