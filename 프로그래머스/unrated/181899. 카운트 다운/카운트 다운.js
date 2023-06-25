4706a0875010a0dfa5e0346d22b2574df94fb083
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-25 22:28:57 +0900

    [unrated] Title: 카운트 다운, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,4 +1,12 @@
 function solution(start, end) {
-    // (start-end+1)개 원소인 배열을 만들어 start에서 인덱스 값을 뺀 값이 원소가 되게 한다.
-    return Array(start - end + 1).fill().map((el, idx) => start - idx)
+    // 방법 2 - for문
+    const answer = []
+    for (let i = start; i >= end; i--) {
+        answer.push(i)
+    }
+    
+    return answer
+    
+    // // 방법 1 - (start-end+1)개 원소인 배열을 만들어 start에서 인덱스 값을 뺀 값이 원소가 되게 한다.
+    // return Array(start - end + 1).fill().map((el, idx) => start - idx)
 }
