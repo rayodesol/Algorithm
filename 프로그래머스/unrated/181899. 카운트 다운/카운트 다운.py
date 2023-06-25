f874571c40101ff1cbd81116aa8debfa9939799d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-25 22:21:28 +0900

    [unrated] Title: 카운트 다운, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
@@ -1,2 +1,6 @@
 def solution(start, end):
-    return [num for num in range(start, end - 1, -1)]
+    # 방법 2 - range
+    return list(range(start, end - 1, -1))
+
+    # # 방법 1 - for문
+    # return [num for num in range(start, end - 1, -1)]
