48b013de441199e7caa647c684678014d185077a
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-26 19:12:42 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,4 +1,7 @@
 function solution(n, t) {
-    // 방법 1 - ** 연산자
-    return n * (2 ** t)
+    // 방법 2 - Math.pow 함수
+    return n * Math.pow(2, t)
+    
+    // // 방법 1 - ** 연산자
+    // return n * (2 ** t)
 }
