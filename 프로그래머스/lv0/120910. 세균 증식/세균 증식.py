f1a4f544cbd0a910270e20ccec0bf7df22eb83b6
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-26 19:10:29 +0900

    [level 0] Title: 세균 증식, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
@@ -1,8 +1,8 @@
-import math
+# import math
 
 def solution(n, t):
-    # 방법 2 - pow 함수
-    return n * math.pow(2, t)
+    # # 방법 2 - pow 함수
+    # return n * math.pow(2, t)
 
-    # # 방법 1 - ** 연산자
-    # return n * (2 ** t)
+    # 방법 1 - ** 연산자
+    return n * (2 ** t)
