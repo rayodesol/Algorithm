856e2c0f5c5e406ef610b9d0af746975a36a419b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-29 16:24:01 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,16 +1,15 @@
 function solution(n) {
+    // 약수의 개수를 구하는 것과 같다.
     let answer = 0
     
-    for (let num = 1; num <= n; num++) {
-        if (n % num === 0) answer += 1
+    // n을 1 ~ n의 제곱근 범위의 수로 나눠 떨어질 때의 수와 그 수로 n을 나눴을 때의 수를 센다.
+    for (let num = 1; num <= Math.sqrt(n); num++) {
+        if (n % num === 0) {
+            answer += 1
+            // 중복되는 수는 제거해서 센다.
+            if (n / num !== num) answer += 1
+        }
     }
-    
-    // for (let num = 1; num <= Math.sqrt(n); num++) {
-    //     if (n % num === 0) {
-    //         answer += 1
-    //         if (n / num !== num) answer += 1
-    //     }
-    // }
     
     return answer
 }
