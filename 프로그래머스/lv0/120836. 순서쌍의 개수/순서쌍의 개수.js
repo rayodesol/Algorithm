8f48ed4498c11f37f2ac21edca600942ca14de87
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-29 16:18:12 +0900

    [level 0] Title: 순서쌍의 개수, Time: 9.30 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,12 +1,16 @@
 function solution(n) {
     let answer = 0
     
-    for (let num = 1; num <= Math.sqrt(n); num++) {
-        if (n % num === 0) {
-            answer += 1
-            if (n / num !== num) answer += 1
-        }
+    for (let num = 1; num <= n; num++) {
+        if (n % num === 0) answer += 1
     }
+    
+    // for (let num = 1; num <= Math.sqrt(n); num++) {
+    //     if (n % num === 0) {
+    //         answer += 1
+    //         if (n / num !== num) answer += 1
+    //     }
+    // }
     
     return answer
 }
