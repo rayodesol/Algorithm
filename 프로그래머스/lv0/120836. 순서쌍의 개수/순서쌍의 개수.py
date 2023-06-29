3378e30cdedfabf813616169081015a543880a37
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-06-29 16:32:43 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,14 +1,14 @@
 def solution(n):
     answer = 0
     
-    # 방법 1 - 1 ~ n 범위 순회
-    for num in range(1, n + 1):
-        if n % num == 0: answer += 1
+    # # 방법 1 - 1 ~ n 범위 순회
+    # for num in range(1, n + 1):
+    #     if n % num == 0: answer += 1
     
     # 방법 2 - 1 ~ n의 제곱근 범위 순회
-    # for num in range(1, int(n ** (1 / 2)) + 1):
-    #     if n % num == 0: 
-    #         answer += 1
-    #         if n / num != num: answer += 1
+    for num in range(1, int(n ** (1 / 2)) + 1):
+        if n % num == 0: 
+            answer += 1
+            if n / num != num: answer += 1
         
     return answer
