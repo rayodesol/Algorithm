35d666228bc8b171eb04e57d37e8c4a6a07e1926
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-01 21:45:47 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,9 +1,9 @@
 def solution(num_list):
-    # filter 함수
-    even = ''.join(map(str, filter(lambda x: x % 2 == 0, num_list)))
-    odd = ''.join(map(str, filter(lambda x: x % 2 == 1, num_list)))
+    # # filter 함수
+    # even = ''.join(map(str, filter(lambda x: x % 2 == 0, num_list)))
+    # odd = ''.join(map(str, filter(lambda x: x % 2 == 1, num_list)))
     
     # 리스트 컴프리핸션
-    # even = ''.join([str(num) for num in num_list if num % 2 == 0])
-    # odd = ''.join([str(num) for num in num_list if num % 2 == 1])
+    even = ''.join([str(num) for num in num_list if num % 2 == 0])
+    odd = ''.join([str(num) for num in num_list if num % 2 == 1])
     return int(even) + int(odd)
