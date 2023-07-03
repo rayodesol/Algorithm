e3cf2e850b7f7291ca87cd0b032811fe033a1da0
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-03 19:00:19 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,4 +1,10 @@
 function solution(myString) {
-    // toLowerCase 함수로 모두 소문자로 바꾼 후, replaceAll 함수로 모든 a를 A로 바꾼다.
-    return myString.toLowerCase().replaceAll('a', 'A')
+    // 방법 2 - 전개 연산자, map 함수, 삼항 연산자, join 함수 사용
+    // 배열로 만들어 map 함수로 A이거나 a일 경우 a로, 아닐 경우 소문자로 바꾸고, 문자열로 바꾼다.
+    return [...myString].map(el => {
+        return el === 'a' || el === 'A'? 'A' : el.toLowerCase()
+    }).join('')
+    
+    // // 방법 1 - toLowerCase 함수로 모두 소문자로 바꾼 후, replaceAll 함수로 모든 a를 A로 바꾼다.
+    // return myString.toLowerCase().replaceAll('a', 'A')
 }
