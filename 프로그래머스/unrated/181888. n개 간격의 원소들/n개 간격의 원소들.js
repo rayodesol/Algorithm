a2bcf29a5a2917c0c739d8ae153ffa32769d0cba
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-07 17:14:29 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,12 +1,12 @@
 function solution(num_list, n) {
-    // 방법 2 - for문
-    const answer = []
+//     // 방법 2 - for문
+//     const answer = []
     
-    for (let i = 0; i < num_list.length; i += n)
-        answer.push(num_list[i])
+//     for (let i = 0; i < num_list.length; i += n)
+//         answer.push(num_list[i])
     
-    return answer
+//     return answer
     
-    // // 방법 1 - filter 함수로 인덱스가 n의 배수인 것만 남긴다.
-    // return num_list.filter((el, idx) => idx % n === 0)
+    // 방법 1 - filter 함수로 인덱스가 n의 배수인 것만 남긴다.
+    return num_list.filter((el, idx) => idx % n === 0)
 }
