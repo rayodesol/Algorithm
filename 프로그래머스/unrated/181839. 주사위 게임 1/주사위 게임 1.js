69e897e75e2fcb39403a30a9cd6faeb57329108e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-08 17:43:58 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,9 +1,9 @@
 function solution(a, b) {
     if (a % 2 && b % 2) { // 모두 홀수
         return a ** 2 + b ** 2
-    } else if (!(a % 2) && !(b % 2)) { // 모두 짝수
-        return Math.abs(a - b)
-    } else {
+    } else if (a % 2 || b % 2) { // 홀수 1개
         return 2 * (a + b)
+    } else {
+        return Math.abs(a - b)
     }
 }
