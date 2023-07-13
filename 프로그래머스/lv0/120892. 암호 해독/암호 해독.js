5fa0b688a94b14b6d1b0ddbbfd36e2b3f9af4a33
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-13 17:00:13 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,14 +1,14 @@
 function solution(cipher, code) {
-    // 방법 2 - for문
-    let answer = ''
+//     // 방법 2 - for문
+//     let answer = ''
     
-    for (let i = code - 1; i < cipher.length; i += code) {
-        answer += cipher[i]
-    }
+//     for (let i = code - 1; i < cipher.length; i += code) {
+//         answer += cipher[i]
+//     }
     
-    return answer
+//     return answer
     
-    // // 방법 1 - 전개 연산자, filter, join 함수 사용.
-    // // 인덱스를 code로 나눴을 때 code-1인 경우의 글자를 빼낸다.
-    // return [...cipher].filter((el, idx) => idx % code === code -1).join('')
+    // 방법 1 - 전개 연산자, filter, join 함수 사용.
+    // 인덱스를 code로 나눴을 때 code-1인 경우의 글자를 빼낸다.
+    return [...cipher].filter((el, idx) => idx % code === code -1).join('')
 }
