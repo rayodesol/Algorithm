cfdea3801fdb524c42ff998650541b9fe6620f5a
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-13 16:52:07 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,7 +1,12 @@
 def solution(cipher, code):
-    answer = ''
+    # 방법 2 - 리스트 컴프리핸션, join 함수 사용. 인덱스를 code-1부터 시작해서 code씩 더해간다.
+    return ''.join([cipher[i] for i in range(code - 1, len(cipher), code)])
+
+#     # 방법 1 - for문, enumerate 함수 사용
+#     answer = ''
     
-    for idx, ch in enumerate(cipher):
-        if idx % code == code - 1: answer += ch
+#     # n번째는 인덱스로 n-1이므로 인덱스를 code로 나눈 나머지가 code-1일 때의 글자를 합한다.
+#     for idx, ch in enumerate(cipher):
+#         if idx % code == code - 1: answer += ch
     
-    return answer
+#     return answer
