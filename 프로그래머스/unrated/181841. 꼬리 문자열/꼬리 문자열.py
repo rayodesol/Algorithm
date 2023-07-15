2fde929f9e0e5dc23c27d05ea3e49439f4cf7552
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-16 08:32:09 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,8 +1,12 @@
 def solution(str_list, ex):
-    answer = ''
+    # 방법 2 - 리스트 컴프리핸션, join 함수
+    return ''.join([string for string in str_list if ex not in string])
     
-    for s in str_list:
-        if ex not in s:
-            answer += s
+#     # 방법 1 - for문
+#     answer = ''
+    
+#     for s in str_list:
+#         if ex not in s:
+#             answer += s
         
-    return answer
+#     return answer
