130af4049c431b34fa3be9f4fbd08bdf3bbd457e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-17 22:41:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,10 @@
+function solution(rsp) {
+    // for문, 삼항 연산자 사용.
+    let answer = ''
+    
+    for (el of rsp) {
+        answer += el === '2' ? '0' : el === '0' ? '5' : '2'
+    }
+    
+    return answer
+}
