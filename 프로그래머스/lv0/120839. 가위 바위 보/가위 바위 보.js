0d03b3df32328912c5d3f35c15cf74bd21927560
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-17 22:47:46 +0900

    [level 0] Title: 가위 바위 보, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,10 +1,19 @@
 function solution(rsp) {
-    // for문, 삼항 연산자 사용.
-    let answer = ''
-    
-    for (let el of rsp) {
-        answer += el === '2' ? '0' : el === '0' ? '5' : '2'
+    // 방법 2 - 객체, 전개 연산자, map, join 사용
+    const win = {
+        '0': '5',
+        '2': '0',
+        '5': '2'
     }
     
-    return answer
+    return [...rsp].map(el => win[el]).join('')
+    
+//     // 방법 1 - for문, 삼항 연산자 사용
+//     let answer = ''
+    
+//     for (let el of rsp) {
+//         answer += el === '2' ? '0' : el === '0' ? '5' : '2'
+//     }
+    
+//     return answer
 }
