81c9d6941fc6e7dfa1183b70e20f2b1b918be55f
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-17 22:59:26 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,5 +1,9 @@
 def solution(rsp):
-    # 딕셔너리, map, lambda, join 사용
+    # 딕셔너리로 이기는 경우를 짝지어 놓는다
     dic = {'0': '5', '2': '0', '5': '2'}
     
-    return ''.join(map(lambda x: dic[x], rsp))
+    # 방법 2 - 리스트 컴프리핸션, join 사용
+    return ''.join([dic[el] for el in rsp])
+    
+    # # 방법 1 - map, lambda, join 사용
+    # return ''.join(map(lambda x: dic[x], rsp))
