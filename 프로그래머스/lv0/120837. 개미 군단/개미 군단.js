5af5b60f6033c7610c44f173c1f4315d0e7dd80f
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-18 23:21:46 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,13 @@
+function solution(hp) {
+    // 방법 1
+    let answer = 0
+    
+    answer += parseInt(hp / 5)  // 장군개미 수(병력을 장군개미 공격력으로 나눈 몫)
+    hp %= 5                     // 남은 병력(병력을 장군개미 공격력으로 나눈 나머지)
+    answer += parseInt(hp / 3)  // 병정개미 수(남은 병력을 병정개미 공격력으로 나눈 몫)
+    hp %= 3                     // 남은 병력(남은 병력을 병정개미 공격력으로 나눈 나머지)
+    answer += hp                // 일개미 공격력이 1이므로 남은 병력이 필요한 일개미의 수
+    
+    return answer
+    
+}
