1b338458fa5f81c5853dc014ec3f3e3cbb82e08b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-20 18:47:46 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,9 +1,7 @@
 function solution(a, b) {
     // a, b를 각각 문자열로 바꿔 +로 합친 뒤 다시 숫자로 바꿔 크기를 비교한다.
-    const strA = String(a)
-    const strB = String(b)
-    const ab = Number(strA + strB)
-    const ba = Number(strB + strA)
+    const ab = Number(String(a) + String(b))
+    const ba = Number(String(b) + String(a))
     
     return ab > ba ? ab : ba
 }
