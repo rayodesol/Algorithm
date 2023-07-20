0476cf542d0ec415549df70b59ee73ef579405b3
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-20 18:50:20 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,7 +1,10 @@
 function solution(a, b) {
-    // a, b를 각각 문자열로 바꿔 +로 합친 뒤 다시 숫자로 바꿔 크기를 비교한다.
-    const ab = Number(String(a) + String(b))
-    const ba = Number(String(b) + String(a))
+    // 방법 2 - 백틱, Math.max 함수 사용
+    return Math.max(`${a}${b}`, `${b}${a}`)
     
-    return ab > ba ? ab : ba
+//     // 방법 1 - a, b를 각각 문자열로 바꿔 +로 합친 뒤 다시 숫자로 바꿔 크기를 비교한다.
+//     const ab = Number(String(a) + String(b))
+//     const ba = Number(String(b) + String(a))
+    
+//     return ab > ba ? ab : ba
 }
