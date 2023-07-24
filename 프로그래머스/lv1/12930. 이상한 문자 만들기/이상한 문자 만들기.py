709a48c9817014d66510d2924e3644be4acee38a
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-24 15:23:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,8 +1,12 @@
 def solution(s):
-    words = s.split(' ')
+    # 방법 2
+    return ' '.join(map(lambda x: ''.join([ch.lower() if i % 2 else ch.upper() for i, ch in enumerate(x)]), s.split(' ')))
+
+#     # 방법 1
+#     words = s.split(' ')
     
-    for i in range(len(words)):
-        words[i] = ''.join([ch.lower() if idx % 2 else ch.upper() for idx, ch in enumerate(words[i])])
+#     for i in range(len(words)):
+#         words[i] = ''.join([ch.lower() if idx % 2 else ch.upper() for idx, ch in enumerate(words[i])])
     
-    return ' '.join(words)
+#     return ' '.join(words)
         
