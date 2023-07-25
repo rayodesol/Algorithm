1919f16e51996c364f615dc1be7fc5cf5a07551b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-26 03:37:32 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -2,13 +2,12 @@ from itertools import permutations
 
 def solution(k, dungeons):
     n = len(dungeons)
-    cases = list(permutations(dungeons, len(dungeons)))
-    max_cnt = 0
+    cases = list(permutations(dungeons, n)) # 순열(탐험하는 순서가 다른 모든 조합)
+    max_cnt = 0 # 최대 던전 수(답)
     
-    for case in cases:
-        cnt = 0
-        state = k
-        # print(case)
+    for case in cases: # 각 경우마다
+        cnt = 0 # 탐험한 던전 수
+        state = k # 남은 피로도
         for i in range(n):
             if state >= case[i][0]:
                 cnt += 1
