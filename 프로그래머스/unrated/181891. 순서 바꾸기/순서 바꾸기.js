a9a1b2f7a86d125bb769ea81e6e3538812f733ce
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-07-31 23:22:00 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,8 +1,9 @@
 function solution(num_list, n) {
+    // 방법 1
     const answer = []
     
-    answer.push(...num_list.splice(n))
-    answer.push(...num_list.splice(0, n))
+    answer.push(...num_list.slice(n))
+    answer.push(...num_list.slice(0, n))
     
     return answer
 }
