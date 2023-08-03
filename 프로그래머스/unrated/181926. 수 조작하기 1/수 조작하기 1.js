c844a87a54385fe3eb777a81b8a0c8c46b456442
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-03 17:43:39 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.21 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,12 +1,27 @@
 function solution(n, control) {
-    // 객체의 키를 알파벳으로, 값을 n에 더할 값을 설정
-    const obj = {
-        'w': 1,
-        's': -1,
-        'd': 10,
-        'a': -10
-    }
+    // 방법 2 - reduce 함수로 각 알파벳에 접근하고, switch 함수로 각 알파벳에 맞는 연산을 한다.
+    return [...control].reduce((acc, el) => {
+        switch(el) {
+            case 'w':
+                return acc += 1
+            case 's':
+                return acc -= 1
+            case 'd':
+                return acc += 10
+            case 'a':
+                return acc -= 10
+        }
+    }, n)
     
-    // reduce 함수로 각 알파벳에 해당하는 값을 n에 더한다.
-    return [...control].reduce((acc, el) => acc += obj[el], n)
+//     // 방법 1
+//     // 객체의 키를 알파벳으로, 값을 n에 더할 값을 설정
+//     const obj = {
+//         'w': 1,
+//         's': -1,
+//         'd': 10,
+//         'a': -10
+//     }
+    
+//     // reduce 함수로 각 알파벳에 해당하는 값을 n에 더한다.
+//     return [...control].reduce((acc, el) => acc += obj[el], n)
 }
