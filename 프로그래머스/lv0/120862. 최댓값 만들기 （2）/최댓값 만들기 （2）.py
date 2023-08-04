3b10d4a1c0836a4c7426ad5067a266f04f8cd85d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-05 02:29:46 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,11 +1,17 @@
-import sys
-
+# 방법 2 - sort 함수로 정렬 후, 맨 앞 두 수의 곱과 맨 뒤 두 수의 곱 중 큰 값을 리턴한다.
 def solution(numbers):
-    answer = -sys.maxsize
-    n = len(numbers)
+    numbers.sort()
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers [-2])
     
-    for i in range(n):
-        for j in range(i + 1, n):
-            answer = max(numbers[i] * numbers[j], answer)
+# # 방법 1 - 완전탐색
+# import sys
+
+# def solution(numbers):
+#     answer = -sys.maxsize
+#     n = len(numbers)
     
-    return answer
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             answer = max(numbers[i] * numbers[j], answer)
+    
+#     return answer
