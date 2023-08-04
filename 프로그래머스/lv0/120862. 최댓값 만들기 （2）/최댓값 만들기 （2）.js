93a52ab74ac1689e7288a0bb4fa923ff3ae9489c
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-05 02:40:43 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,8 +1,19 @@
 function solution(numbers) {
-    // 방법 1 - sort 함수로 정렬 후, 맨 앞 두 수 곱과 맨 뒤 두 수 곱 중 큰 값을 리턴한다.
-    const n = numbers.length
+    // 방법 2 - 완전탐색
+    let answer = -(10 ** 8) // 문제 범위내에서 나올 수 있는 가장 최솟값으로 초기화
     
-    numbers.sort((a, b) => a - b)
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            answer = Math.max(answer, numbers[i] * numbers[j])
+        }
+    }
     
-    return Math.max(numbers[0] * numbers[1], numbers[n - 1] * numbers[n - 2])
+    return answer
+    
+//     // 방법 1 - sort 함수로 정렬 후, 맨 앞 두 수 곱과 맨 뒤 두 수 곱 중 큰 값을 리턴한다.
+//     const n = numbers.length
+    
+//     numbers.sort((a, b) => a - b)
+    
+//     return Math.max(numbers[0] * numbers[1], numbers[n - 1] * numbers[n - 2])
 }
