5a79fe8749b35f50b0ce702036b788887b898ccd
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-04 23:48:14 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,8 +1,12 @@
 def solution(box, n):
-    # 방법 1
-    answer = 1
+    # 방법 2
+    x, y, z = box
+    return (x // n) * (y // n) * (z // n)
+
+#     # 방법 1
+#     answer = 1
     
-    for length in box:
-        answer *= length // n
+#     for length in box:
+#         answer *= length // n
     
-    return answer
+#     return answer
