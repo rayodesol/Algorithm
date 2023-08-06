2b13106aafe77c7bba49b07c34cafae1f0ebc9f9
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-06 23:39:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -13,17 +13,17 @@
 
 #     return ''.join(map(str, list(queue))) # 문자열 형태로 리턴
 
-# 이진수로 변환하는 함수 - 2
-def binaryNum(num):
-    arr = [] # 배열 사용
+# # 이진수로 변환하는 함수 - 2
+# def binaryNum(num):
+#     arr = [] # 배열 사용
     
-    while num != 1: # 마지막 몫이 1일 될 때까지
-        arr.append(num % 2) # 2로 나눈 나머지 추가
-        num //= 2 # 2로 나누기
+#     while num != 1: # 마지막 몫이 1일 될 때까지
+#         arr.append(num % 2) # 2로 나눈 나머지 추가
+#         num //= 2 # 2로 나누기
         
-    arr.append(1) # 마지막 몫 1 추가
-    arr.reverse() # 각 숫자가 만들어진 순서와 이진수로 나열되는 수가 역순이므로 역순 정렬
-    return ''.join(map(str, arr)) # 문자열 형태로 리턴
+#     arr.append(1) # 마지막 몫 1 추가
+#     arr.reverse() # 각 숫자가 만들어진 순서와 이진수로 나열되는 수가 역순이므로 역순 정렬
+#     return ''.join(map(str, arr)) # 문자열 형태로 리턴
         
 def solution(s):
     answer = [0, 0] # [변환 횟수, 0 개수]
@@ -32,7 +32,8 @@ def solution(s):
         zero = s.count('0') # 제거되는 0의 개수
         answer[1] += zero
         
-        s = binaryNum(len(s) - zero) # 1의 개수로 이진수 만들기
+        # s = binaryNum(len(s) - zero) # 1의 개수로 이진수 만들기
+        s = format(len(s) - zero, 'b') # 방법 3 - format 함수로 이진수 만들기
         answer[0] += 1 # 이진 변환 횟수 증가
     
     return answer
