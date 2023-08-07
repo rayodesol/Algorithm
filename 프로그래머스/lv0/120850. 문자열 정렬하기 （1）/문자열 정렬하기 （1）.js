83258e06d69b62a830dab1c4cc37650a7919f8e6
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-07 17:28:31 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,3 +1,5 @@
 function solution(my_string) {
+    // filter, isNaN 함수로 숫자인 글자만 고른 후, map, Number 함수로 숫자로 만들고,
+    // sort 함수로 오름차순 정렬한다.
     return [...my_string].filter(el => !isNaN(el)).map(el => Number(el)).sort((a, b) => a - b)
 }
