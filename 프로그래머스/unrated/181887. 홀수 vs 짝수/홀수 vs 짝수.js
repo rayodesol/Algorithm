19335dfa93d0b9c8db1ffcb5c718f8c6553e4cf0
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-10 16:28:18 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,10 +1,16 @@
 function solution(num_list) {
-    let odd = 0
-    let even = 0
-    
-    for (let i = 0; i < num_list.length; i++) {
-        i % 2 ? even += num_list[i] : odd += num_list[i]
-    }
-    
+    // 방법 2 - filter, reduce 함수
+    const odd = num_list.filter((el, i) => !(i % 2)).reduce((acc, el) => acc += el, 0)
+    const even = num_list.filter((el, i) => i % 2).reduce((acc, el) => acc += el, 0)
     return odd > even ? odd : even
+    
+//     // 방법 1 - for문
+//     let odd = 0
+//     let even = 0
+    
+//     for (let i = 0; i < num_list.length; i++) {
+//         i % 2 ? even += num_list[i] : odd += num_list[i]
+//     }
+    
+//     return odd > even ? odd : even
 }
