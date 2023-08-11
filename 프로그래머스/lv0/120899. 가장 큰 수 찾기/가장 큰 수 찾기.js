74846192004d90251609a8a382cbb70763c7a90f
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-12 01:35:37 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,12 +1,17 @@
 function solution(array) {
-    const answer = [0, 0]
+    // 방법 2 - Math.max, indexOf 함수
+    const max_num = Math.max(...array)
+    return [max_num, array.indexOf(max_num)]
     
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > answer[0]) {
-            answer[0] = array[i]
-            answer[1] = i
-        }
-    }
+//     // 방법 1 - for문
+//     const answer = [0, 0]
     
-    return answer
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] > answer[0]) {
+//             answer[0] = array[i]
+//             answer[1] = i
+//         }
+//     }
+    
+//     return answer
 }
