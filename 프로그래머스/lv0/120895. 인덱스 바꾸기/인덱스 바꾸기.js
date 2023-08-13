9e7ade41978923d955361ecbb6b33ba5886dbea0
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-13 22:22:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,6 +1,6 @@
 function solution(my_string, num1, num2) {
     // 방법 2 - 리스트로 바꾼 후 글자 바꾸기, 구조분해할당 사용
-    let arr = my_string.split('');
+    let arr = [...my_string];
     [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
     return arr.join('');
     
