347f84321c6b9728d669c327b56ba489d194949e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-22 23:37:33 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,9 +1,13 @@
 def solution(binomial):
-    arr = binomial.split(' ')
+    # 방법 2 - eval 함수
+    return eval(binomial)
+
+#     # 방법 1 - 문자열을 띄어쓰기 기준으로 숫자와 연산자 분리 후, if-elif문으로 연산자에 따라 처리
+#     arr = binomial.split(' ')
     
-    if arr[1] == '+':
-        return int(arr[0]) + int(arr[2])
-    elif arr[1] == '-':
-        return int(arr[0]) - int(arr[2])
-    else:
-        return int(arr[0]) * int(arr[2])
+#     if arr[1] == '+':
+#         return int(arr[0]) + int(arr[2])
+#     elif arr[1] == '-':
+#         return int(arr[0]) - int(arr[2])
+#     else:
+#         return int(arr[0]) * int(arr[2])
