3ee23f044b552e71c2de7d751f79895e9c8e0e6e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-25 23:37:04 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,17 +1,17 @@
 function solution(order) {
-    // 방법 2 - 문자열로 바꿔 전개 연산자로 각 숫자로 이루어진 배열을 만든다.
-    // filter함수로 3, 6, 9일 경우만 남기고, length함수로 개수를 센다.
-    return [...String(order)].filter(el => el === '3' || el === '6' || el === '9').length
+    // // 방법 2 - 문자열로 바꿔 전개 연산자로 각 숫자로 이루어진 배열을 만든다.
+    // // filter함수로 3, 6, 9일 경우만 남기고, length함수로 개수를 센다.
+    // return [...String(order)].filter(el => el === '3' || el === '6' || el === '9').length
     
-//     // 방법 1 - 문자열로 바꾼 후 각 숫자를 순회하며 3, 6, 9의 개수를 센다.
-//     let cnt = 0
-//     const str = String(order)
+    // 방법 1 - 문자열로 바꾼 후 각 숫자를 순회하며 3, 6, 9의 개수를 센다.
+    let cnt = 0
+    const str = String(order)
     
-//     for (let i = 0; i < str.length; i++) {
-//         if (str[i] === '3' || str[i] === '6' || str[i] === '9') {
-//             cnt += 1
-//         }
-//     }
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '3' || str[i] === '6' || str[i] === '9') {
+            cnt += 1
+        }
+    }
     
-//     return cnt
+    return cnt
 }
