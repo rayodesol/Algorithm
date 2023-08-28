1c2a118e2348cb29a60614fc22756061c669af11
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-28 17:00:26 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,4 +1,4 @@
 function solution(my_string) {
     // 아스키코드로 대소문자를 구분 후 toUpperCase, toLowerCase 함수로 대소문자를 변환한다.
-    return [...my_string].map(el => el.charCodeAt() >= 65 &&  el.charCodeAt() <= 90 ? el.toLowerCase() : el.toUpperCase()).join('')
+    return [...my_string].map(el => el.charCodeAt() <= 90 ? el.toLowerCase() : el.toUpperCase()).join('')
 }
