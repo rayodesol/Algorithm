957c5adee552ea7aa06f03c926c6bd02e5d01791
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-08-30 14:36:02 +0900

    [level 2] Title: 타겟 넘버, Time: 2.94 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,4 +1,4 @@
-def dfs(numbers, step, total, target):    
+def dfs(numbers, step, total, target):
     if step == len(numbers): return 1 if total == target else 0
     return dfs(numbers, step + 1, total + numbers[step], target) + dfs(numbers, step + 1, total - numbers[step], target)
 
