1d9cc0eade94f3686fe4f623f6a974058de5c401
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-01 20:09:17 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 11.51 ms, Memory: 7.38 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
@@ -1,11 +1,11 @@
 n = int(input())
 
-# 방법 2 - for문
-for i in range(1, n + 1):
-    print('*' * i)
-
-# # 방법 1 - 이중 for문
+# # 방법 2 - for문
 # for i in range(1, n + 1):
-#     for _ in range(i):
-#         print('*', end = '')
-#     print()
+#     print('*' * i)
+
+# 방법 1 - 이중 for문
+for i in range(1, n + 1):
+    for _ in range(i):
+        print('*', end = '')
+    print()
