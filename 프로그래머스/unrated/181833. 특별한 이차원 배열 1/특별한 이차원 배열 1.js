fdfa11c08d716dd45c75e070e566b6adf48b575c
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-03 10:13:40 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.19 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,6 +1,8 @@
 function solution(n) {
+    // 모든 원소가 0인 n행 n열 이중배열을 선언한다.
     const answer = Array.from(Array(n), () => Array(n).fill(0))
     
+    // 행, 열의 인덱스가 같은 원소만 1로 바꾼다.
     for (let i = 0; i < n; i++) {
         answer[i][i] = 1
     }
