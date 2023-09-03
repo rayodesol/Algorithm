c28c7fe98c4f1ee52118f64c0bac9582d46ec166
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-03 10:05:02 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.18 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const answer = Array.from(Array(n), () => Array(n).fill(0))
+    
+    for (let i = 0; i < n; i++) {
+        answer[i][i] = 1
+    }
+    
+    return answer
+}
