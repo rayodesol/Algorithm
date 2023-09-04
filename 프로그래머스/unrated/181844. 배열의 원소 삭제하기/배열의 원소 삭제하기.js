57dba88c6bc0e89183540f4eb799a486b0171ab7
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-04 18:32:35 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,12 +1,16 @@
 function solution(arr, delete_list) {
-    const answer = []
+    // 방법 2 - filter함수를 사용해 delete_list에 포함되지 않는 원소만 남긴다.
+    return arr.filter(el => !delete_list.includes(el))
     
-    // arr의 원소를 순회하며 각 단어가 delete_list에 없을 때만 빈 배열에 단어를 추가한다.
-    for (let word of arr) {
-        if (!delete_list.includes(word)) {
-            answer.push(word)
-        }
-    }
+//     // 방법 1 - for문
+//     const answer = []
     
-    return answer
+//     // arr의 원소를 순회하며 각 단어가 delete_list에 없을 때만 빈 배열에 단어를 추가한다.
+//     for (let word of arr) {
+//         if (!delete_list.includes(word)) {
+//             answer.push(word)
+//         }
+//     }
+    
+//     return answer
 }
