baa7dec807751a432712be6cea4991bd516da14d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-04 18:26:23 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,9 +1,13 @@
 def solution(arr, delete_list):
-    answer = []
+    # 방법 2 - 방법 1를 리스트 컴프리헨션으로
+    return [word for word in arr if word not in delete_list]
+
+#     # 방법 1
+#     answer = []
     
-    # arr의 원소를 순회하며 각 단어가 delete_list에 없을 때만 빈 배열에 단어를 추가한다.
-    for word in arr:
-        if word not in delete_list:
-            answer.append(word)
+#     # arr의 원소를 순회하며 각 단어가 delete_list에 없을 때만 빈 배열에 단어를 추가한다.
+#     for word in arr:
+#         if word not in delete_list:
+#             answer.append(word)
             
-    return answer
+#     return answer
