29c99c6393533b69daaf1bea3a153b685c1b8635
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-08 00:02:58 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,6 +1,3 @@
 def solution(myString, pat):
     # myString과 pat 모두 대문자로 바꾼 뒤, in 연산자로 포함 여부를 가린다.
-    myString = myString.upper()
-    pat = pat.upper()
-    
-    return 1 if pat in myString else 0
+    return 1 if pat.upper() in myString.upper() else 0
