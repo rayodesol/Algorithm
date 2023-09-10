1b9bc16e0f5c03ad06fc8a272370f0160edae370
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-10 21:58:03 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,3 +1,4 @@
 def solution(myString, pat):
+    # pat을 A는 B로, B는 A로 바꾼 후, myString에 포함되는지 확인한다.
     changedPat = ''.join(['B' if ch == 'A' else 'A' for ch in pat])
     return 1 if changedPat in myString else 0
