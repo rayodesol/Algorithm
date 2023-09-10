6700fc735f1d8bd96afacb8c3508943802ae73da
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-10 22:06:55 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,3 +1,8 @@
 def solution(myString, pat):
-    # pat을 A는 B로, B는 A로 바꾼 후, myString에 포함되는지 확인한다.
-    return int(''.join(['B' if ch == 'A' else 'A' for ch in pat]) in myString)
+    # 방법 2
+    # replace 사용. pat을 A -> C, B -> A, C -> B 로 바꾼 뒤, myString에 포함되는지 확인한다.
+    return int(pat.replace('A', 'C').replace('B', 'A').replace('C', 'B') in myString)
+
+    # # 방법 1
+    # # pat을 A는 B로, B는 A로 바꾼 후, myString에 포함되는지 확인한다.
+    # return int(''.join(['B' if ch == 'A' else 'A' for ch in pat]) in myString)
