3030c9f20037488877ba0b4cbe04833b7a39c75b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-11 22:41:27 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -1,9 +1,9 @@
 def solution(arr):
-    # 이중 for문 사용. arr를 순회하며, 각 숫자 번씩 반복하며 각 숫자를 추가한다.
+    # 방법 1 - 이중 for문 사용. arr를 순회하며, 각 숫자 번씩 반복하며 각 숫자를 추가한다.
     answer = []
     
     for num in arr:
-        for i in range(num):
+        for _ in range(num):
             answer.append(num)
             
     return answer
