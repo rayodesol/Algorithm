58a74682fbcdbc53088e45c53fb93b688da6caf6
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-13 08:33:29 +0900

    [Gold V] Title: 개똥벌레, Time: 588 ms, Memory: 55012 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -3,18 +3,19 @@ import sys
 Input = sys.stdin.readline
 N, H = map(int, Input().split())
 odd, even = [], []
-cnt = [0 for _ in range(H)]
+cnt = [0 for _ in range(H)] # 구간별 장애물 수
 
+# 종유석끼리, 석순끼리 따로 저장
 for i in range(N):
   l = int(Input())
-  if i % 2:
+  if i % 2: # 종유석
     odd.append(l - 1)
-  else:
+  else: # 석순
     even.append(l - 1)
 odd.sort(reverse = True)
 even.sort(reverse = True)
 
-# 종유석
+# 구간별 종유석의 수
 for i in range(H):
   if not odd: break
   if i < odd[-1]:
@@ -24,7 +25,7 @@ for i in range(H):
     while odd and i == odd[-1]:
       odd.pop()
 
-# 석순
+# 구간별 석순의 수
 for i in range(H - 1, 0, -1):
   if not even: break
   if H - 1 - i < even[-1]:
