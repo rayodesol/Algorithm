be3551af3a1cc79654df691b27e6922ef17f2616
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-18 23:59:38 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,6 @@
+def solution(my_string, overwrite_string, s):
+    # 슬라이싱 사용
+    # 앞부분은 my_string의 인덱스 0 ~ (s-1)
+    # 중간 부분은 overwrite_string 전체
+    # 끝부분은 my_string에서 앞 (s + 1 + overwrite_string의 길이)개가 빠진 끝까지
+    return my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
