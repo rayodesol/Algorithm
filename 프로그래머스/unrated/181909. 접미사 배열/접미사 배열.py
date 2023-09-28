73397136ef5ff3f6b9bb973c1d74cb51418bf2a0
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-09-28 23:52:09 +0900

    [unrated] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
@@ -1,8 +1,12 @@
 def solution(my_string):
-    answer = []
+    # 방법 2 - 방법1을 리스트 컴프리헨션으로
+    return sorted([my_string[i:] for i in range(len(my_string))])
+
+#     # 방법 1
+#     answer = []
     
-    # 0부터 인덱스 끝까지를 시작점으로 슬라이싱한 문자열을 배열에 추가한다
-    for i in range(len(my_string)):
-        answer.append(my_string[i:])
+#     # 0부터 인덱스 끝까지를 시작점으로 슬라이싱한 문자열을 배열에 추가한다
+#     for i in range(len(my_string)):
+#         answer.append(my_string[i:])
     
-    return sorted(answer)   # sorted함수로 정렬한다
+#     return sorted(answer)   # sorted함수로 정렬한다
