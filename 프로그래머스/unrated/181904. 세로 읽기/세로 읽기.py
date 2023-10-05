fe7bbaf4ff8995bd0e0086f548f2892c66289ec4
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-10-05 23:03:03 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -2,6 +2,8 @@ def solution(my_string, m, c):
     answer = ''
     i = 0
     
+    # m 글자씩 자르면, 맨 앞 글자가 인덱스가 (m의 배수)인 글자가 된다
+    # 이 상태에서 c번째 열은, 인덱스가 ((m의 배수) + (c - 1))인 글자이다
     while True:
         if m * i + c - 1 >= len(my_string): break
         answer += my_string[m * i + c - 1]
