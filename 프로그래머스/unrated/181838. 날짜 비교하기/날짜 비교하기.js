3ba6ed8b47078c9252d2f4bbe89133fb2adaf1b2
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-10-09 23:59:49 +0900

    Update 날짜 비교하기.js

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,5 +1,5 @@
 function solution(date1, date2) {
-    // 연도, 월, 일 순으로 비교한다
+    // 연도, 월, 일 순으로 비교한다.
     if (date1[0] < date2[0]) return 1
     else if (date1[0] > date2[0]) return 0
     else if (date1[1] < date2[1]) return 1
