1792eda9dae2eb4870fedd181bae567eeab76c5a
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-10-10 23:47:52 +0900

    [level unrated] Title: 수 조작하기 2, Time: 15.84 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,15 @@
+def solution(numLog):
+    answer = ''
+    
+    for i in range(len(numLog) - 1):
+        diff = numLog[i + 1] - numLog[i]
+        if diff == 1:
+            answer += 'w'
+        elif diff == -1:
+            answer += 's'
+        elif diff == 10:
+            answer += 'd'
+        else:
+            answer += 'a'
+    
+    return answer
