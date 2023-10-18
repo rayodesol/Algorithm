183473fe2044c911669a4c4117759e7b8f0e4751
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-10-18 22:30:34 +0900

    [level 2] Title: 주식가격, Time: 22.92 ms, Memory: 17.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,11 @@
+def solution(prices):
+    answer = [i for i in range(len(prices) - 1, -1, -1)]
+    stack = []
+    
+    for idx, price in enumerate(prices):
+        while stack and stack[-1][0] > price:
+            answer[stack[-1][1]] = idx - stack[-1][1]
+            stack.pop()
+        stack.append((price, idx))
+    
+    return answer
