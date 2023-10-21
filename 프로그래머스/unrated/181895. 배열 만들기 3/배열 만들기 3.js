48bf23140adfe7e864c2c08be79efc16a8c58dc3
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-10-21 22:39:47 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.71 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,7 +1,5 @@
 function solution(arr, intervals) {
-    let s1, e1, s2, e2;
-    [s1, e1] = intervals[0];
-    [s2, e2] = intervals[1];
+    const [[s1, e1], [s2, e2]] = intervals;
     
     return [...arr.slice(s1, e1 + 1), ...arr.slice(s2, e2 + 1)];
 }
