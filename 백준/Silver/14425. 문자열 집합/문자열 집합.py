b2861744367b37cfe6db8afcd7210e92406a7c7e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-03 15:10:53 +0900

    Update 문자열 집합.py

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -3,10 +3,14 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 
+# 집합 내 포함 여부를 묻는 문제이므로
+# 데이터 중복이 되지 않고, 삽입, 탐색의 시간복잡도가 O(1)인 HashSet 사용
+# n개의 문자열로 이루어진 집합 s 만들기
 s = set()
 for _ in range(n):
   s.add(input())
 
+# m개의 문자열 중 집합 s에 포함되어 있는 것 세기
 answer = 0
 for _ in range(m):
   word = input()
