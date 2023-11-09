acbf766c572cc3068328c3a9ce6032b44e610f91
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-09 23:40:15 +0900

    [level unrated] Title: k의 개수, Time: 10.75 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.py
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.py
@@ -1,7 +1,11 @@
 def solution(i, j, k):
-    answer = 0
+    # 방법 2 - 문자열로 바꿔 count함수 사용
+    return sum([str(num).count(str(k)) for num in range(i, j + 1)])
+
+#     # 방법 1 - 문자열로 바꿔 split으로 k마다 끊어 그 개수 세기
+#     answer = 0
     
-    for num in range(i, j + 1):
-        answer += len(str(num).split(str(k))) - 1
+#     for num in range(i, j + 1):
+#         answer += len(str(num).split(str(k))) - 1
     
-    return answer
+#     return answer
