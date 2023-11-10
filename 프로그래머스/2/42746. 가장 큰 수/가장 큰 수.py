053309f637618e8043b7a2aa11592597ed407fb3
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-10 21:57:57 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,7 @@
 def solution(numbers):
     strings = list(map(str, numbers)) # 문자열로 바꾸기
     
-    # 3번씩 반복하게 만들어, 역순으로 큰것부터 배열
+    # 3번씩 반복한 문자열을 기준으로, 역순으로 큰것부터 배열
     strings.sort(key = lambda s: s * 3, reverse = True)
     
     return str(int(''.join(strings))) # 정렬된 배열 문자열로 만들기
