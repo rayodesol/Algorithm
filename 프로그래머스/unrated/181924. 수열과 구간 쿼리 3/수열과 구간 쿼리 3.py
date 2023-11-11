93686dcd9bf8b505252b75156fe32e986980bae8
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-11 23:27:26 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,7 +1,12 @@
 def solution(arr, queries):
+    # 방법 2
     for x, y in queries:
-        temp = arr[x]
-        arr[x] = arr[y]
-        arr[y] = temp
+        arr[x], arr[y] = arr[y], arr[x]
+    
+    # 방법 1
+    # for x, y in queries:
+    #     temp = arr[x]
+    #     arr[x] = arr[y]
+    #     arr[y] = temp
     
     return arr
