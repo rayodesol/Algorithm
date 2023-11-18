fe7e3e05cdeeb9e00809a2d9d0357bc1674eda8b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-18 20:16:36 +0900

    [level unrated] Title: 7의 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.py
@@ -1,3 +1,6 @@
 def solution(array):
-    # 배열의 각 원소를 문자열로 바꾼 후, count 함수로 문자열에서 7의 개수를 세서 합하기
+    # 방법 2 - array를 문자열로 바꾼 후, count 함수로 '7'의 개수 세기
+    return str(array).count('7')
+
+    # 방법 1 - 배열의 각 원소를 문자열로 바꾼 후, count 함수로 문자열에서 '7'의 개수를 세서 합하기
     return sum([str(num).count('7') for num in array])
