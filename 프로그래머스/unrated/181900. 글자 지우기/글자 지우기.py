58098c94cd7450078c70d5a8493a03a4f960f295
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-21 23:58:56 +0900

    Update 글자 지우기.py

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -2,8 +2,9 @@ def solution(my_string, indices):
     answer = ''
     j = 0
     
-    indices.sort()
-    
+    indices.sort() # 정렬해서 인덱스 순서대로 접근할 수 있게 만든다.
+
+    # indices 배열에 있는 인덱스만 제외하고 모든 문자를 answer 변수의 문자열로 만든다.
     for i in range(len(my_string)):
         if j < len(indices) and i == indices[j]:
             j += 1
