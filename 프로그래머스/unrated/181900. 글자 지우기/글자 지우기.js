64b550e4b0218836189a8411098da4159ea28ffc
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-22 23:57:16 +0900

    Update 글자 지우기.js

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -2,8 +2,9 @@ function solution(my_string, indices) {
     let answer = ''
     let j = 0
     
-    indices.sort((a, b) => a - b)
-    
+    indices.sort((a, b) => a - b) // 정렬해서 인덱스 순서대로 접근할 수 있게 만든다
+
+    // indices 배열에 있는 인덱스만 제외하고 모든 문자를 answer 변수의 문자열로 만든다
     for (let i = 0; i < my_string.length; i++) {
         if (j < indices.length && i === indices[j]) {
             j += 1
