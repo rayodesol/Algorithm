6f620821e1ffffb6fed12101aa69a429d11b7be7
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-25 23:51:04 +0900

    Update 커피 심부름.py

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -2,9 +2,9 @@ def solution(order):
     answer = 0
     
     for menu in order:
-        if 'americano' in menu or menu == 'anything':
+        if 'americano' in menu or menu == 'anything': # 핫/아이스 상관없이 '아메리카노'인 것(in 연산자 사용)과 '아무거나'일 때(== 연산자 사용)
             answer += 4500
-        elif 'cafelatte' in menu:
+        elif 'cafelatte' in menu: # 핫/아이스 상관없이 '카페라떼'일 때(in 연산자 사용)
             answer += 5000
     
     return answer
