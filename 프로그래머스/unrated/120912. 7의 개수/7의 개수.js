b8582fc3aaf57b1fcbbd8a3f4fa5d57eaa9eda1c
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-11-26 23:39:27 +0900

    [level unrated] Title: 7의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.js
@@ -1,7 +1,9 @@
 function solution(array) {
     try {
+        // 정규식으로 특정 문자 개수 세기; "문자열".match(/문자/g).length
         return array.toString().match(/7/g).length
     } catch (e) {
+        // 찾는 문자가 없을 때 예외 처리
         return 0
     }
 }
