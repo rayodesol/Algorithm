64f035df415c96130c700dbe488b385d941b864e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-02 23:44:11 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,4 @@
 function solution(my_string, is_suffix) {
+    // my_string의 맨 끝으로부터 is_suffix의 글자 수만큼의 문자열이 is_suffix와 같은지 비교
     return my_string.slice(-is_suffix.length) === is_suffix ? 1 : 0
 }
