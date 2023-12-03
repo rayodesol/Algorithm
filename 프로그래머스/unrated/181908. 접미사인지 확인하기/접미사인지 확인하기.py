2e208e0398c27bd5da05dc7d16b46fa1cc2f97d8
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-03 17:55:56 +0900

    Update 접미사인지 확인하기.py

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,4 +1,4 @@
 def solution(my_string, is_suffix):
-    # 슬라이싱 사용
-    # my_string의 맨 끝으로부터 is_suffix의 글자 수만큼의 문자열이 is_suffix와 같은지 비교
+    # 슬라이싱 사용.
+    # my_string의 맨 끝으로부터 is_suffix의 글자 수만큼의 문자열이 is_suffix와 같은지 비교.
     return 1 if my_string[-len(is_suffix):] == is_suffix else 0
