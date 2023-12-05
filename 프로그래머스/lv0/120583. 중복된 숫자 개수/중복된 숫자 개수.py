3597a3e309db4bcacde1ad519b3cb15e00593e2e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-05 23:57:15 +0900

    Update 중복된 숫자 개수.py

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -2,5 +2,5 @@ def solution(array, n):
     # 방법 2 - count 함수
     return array.count(n)
 
-    # # 방법 1 - filter 함수로 n과 같은 원소들을 가진 배열의 길이를 리턴한다.
+    # # 방법 1 - filter 함수로 n과 같은 원소들을 가진 배열의 길이를 리턴
     # return len(list(filter(lambda x : x == n, array)))
