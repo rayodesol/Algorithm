7f5d42b397bc45b3c244c55c81ca8314c25c83e9
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-06 23:48:34 +0900

    Update 머쓱이보다 키 큰 사람.js

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,4 +1,4 @@
 function solution(array, height) {
-    // filter 함수로 height보다 큰 값들의 배열을 구한 뒤, 배열의 길이를 리턴한다.
+    // filter 함수로 height보다 큰 값들의 배열을 구한 뒤, 배열의 길이를 리턴
     return array.filter(el => el > height).length
 }
