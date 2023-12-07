db283af413cfeeb3d84630c2aaf6a1aad3c8796b
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-07 23:38:22 +0900

    Update 피자 나눠 먹기 （1）.py

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,8 +1,8 @@
 import math
 
 def solution(n):
-    # 방법 2 - math.ceil 함수로 n을 7로 나눈 값을 올림한다.
+    # 방법 2 - math.ceil 함수로 n을 7로 나눈 값을 올림
     return math.ceil(n / 7)
 
-    # # 방법 1 - 나누어 떨어지는지에 따라 다르게 처리하기
+    # # 방법 1 - 나누어 떨어지는지에 따라 다르게 처리
     # return n // 7 if n % 7 == 0 else n // 7 + 1
