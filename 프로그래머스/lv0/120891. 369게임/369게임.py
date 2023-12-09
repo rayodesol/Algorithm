a24901c38f4416da1582b53376ed84c687cc7ff7
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-09 23:57:13 +0900

    Update 369게임.py

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,4 +1,4 @@
 def solution(order):
-    # count 함수 사용. 3의 개수 + 6의 개수 + 9의 개수 리턴.
+    # count 함수 사용. 3의 개수 + 6의 개수 + 9의 개수 리턴
     string = str(order)
     return string.count('3') + string.count('6') + string.count('9')
