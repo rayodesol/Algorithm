b36cba2356cef347da884eb0032c504fba19b42d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-10 23:59:20 +0900

    Update 세균 증식.py

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
@@ -1,8 +1,8 @@
 # import math
 
 def solution(n, t):
-    # # 방법 2 - pow 함수
+    # # 방법 2 - pow 함수 사용
     # return n * math.pow(2, t)
 
-    # 방법 1 - ** 연산자
+    # 방법 1 - ** 연산자 사용
     return n * (2 ** t)
