ba47fb49db66eb67ea6e2b08514ee5f453370b49
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-13 23:58:23 +0900

    Update 문자열안에 문자열.py

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -2,5 +2,5 @@ def solution(str1, str2):
     # 방법 2 - in 연산자
     return 1 if str2 in str1 else 2
 
-    # # 방법 1 - split 함수로 str1을 str2 기준으로 나눴을 때 나눠지면 str2가 포함되었다는 것임
+    # # 방법 1 - split 함수로 str1을 str2 기준으로 나눴을 때 나눠지면 str2가 포함되었다는 것
     # return 1 if len(str1.split(str2)) > 1 else 2
