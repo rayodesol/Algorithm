30766b6dc5400bff9f0215d8265f66ca8320371e
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-15 23:56:12 +0900

    [level unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -2,8 +2,10 @@ def solution(arr, flag):
     answer = []
     
     for i in range(len(arr)):
+        # true이면, 배열 [arr[i]]를 (arr[i] * 2)번 곱해 원소 개수를 늘린 것을 배열 answer에 더한다
         if flag[i]:
             answer += [arr[i]] * arr[i] * 2
+        # false이면, 배열 answer의 뒷부분 원소 arr[i]개를 슬라이싱한 배열로 바꾼다
         else:
             answer = answer[:-arr[i]]
     
