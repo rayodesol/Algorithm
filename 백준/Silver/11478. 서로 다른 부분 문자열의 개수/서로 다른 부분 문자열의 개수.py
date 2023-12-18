952b09bcb108693e6737adca08d5d95ce7dba3a7
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-18 23:59:38 +0900

    Update 서로 다른 부분 문자열의 개수.py

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,11 +1,11 @@
-# 중복 없이 개수를 세기 위해 중복을 허용하지 않는 HashSet 사용
+# 중복 없이 개수를 세기 위해 중복을 허용하지 않는 HashSet 사용.
 strSet = set()
 s = input()
 
 n = len(s)
-# 부분 문자열 만들기
+# 부분 문자열 만들기.
 for i in range(n):
   for j in range(i, n):
-    strSet.add(s[i:j+1])  # 슬라이싱 사용
+    strSet.add(s[i:j+1])  # 슬라이싱 사용.
 
 print(len(strSet))
