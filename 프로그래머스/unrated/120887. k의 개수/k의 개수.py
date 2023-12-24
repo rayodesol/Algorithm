482548051954456509b1daea8b04bc241254871d
rayodesol <104487178+rayodesol@users.noreply.github.com>
2023-12-24 23:56:33 +0900

    Update k의 개수.py

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.py
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.py
@@ -1,8 +1,8 @@
 def solution(i, j, k):
-    # 방법 2 - 문자열로 바꿔 count함수 사용
+    # 방법 2 - 문자열로 바꿔 count함수 사용.
     return sum([str(num).count(str(k)) for num in range(i, j + 1)])
 
-#     # 방법 1 - 문자열로 바꿔 split으로 k마다 끊어 그 개수 세기
+#     # 방법 1 - 문자열로 바꿔 split으로 k마다 끊어 그 개수 세기.
 #     answer = 0
     
 #     for num in range(i, j + 1):
